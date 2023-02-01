fa32b55cb89444b49f5c16ea5df52ef068e3eba0
Mugdho <saha2171515@stud.kuet.ac.bd>
2023-02-01 12:41:48 +0600

    initial push

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+(function($){
+	$(document).ready(function() {	
+
+		$(document).on('click', '.toggle-password', function() {
+
+		    $(this).toggleClass("fa-eye fa-eye-slash");
+		    
+		    var input = $("#pass_log_id");
+		    input.attr('type') === 'password' ? input.attr('type','text') : input.attr('type','password')
+		});
+
+		
+		
+	});
+})(jQuery);
